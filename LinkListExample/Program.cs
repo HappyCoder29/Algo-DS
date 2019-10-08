7039166301c882e55e3f28a3714308d405629384
happycoder29 <ashish@drillmaps.com>
2019-10-07 22:15:31 -0700

    Checking in Initial code for Link List.

---
+++ LinkListExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinkListExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
